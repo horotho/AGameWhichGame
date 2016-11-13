70bb9d968e3cb2507d0f67d791c26a334711b253
Aaron <mertz.ac@gmail.com>
2016-11-13 01:16:36 -0700

    Update player to be drawn as a simple rectangle. Add in bullets.

--- include/resources.h
+++ include/resources.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-#define BALL_TEXTURE "ball.png"
-
+#define TEXTURE_BALL   "ball.png"
+#define TEXTURE_WACKY  "wacky.png"
+#define TEXTURE_PLAYER "player.png"
--- src/agame.cc
+++ src/agame.cc
@@ -1,4 +1,13 @@
-#include "resources.h"
+//======================================================================================================================
+// Overview
+//======================================================================================================================
+// The main file for A GAME, which contains the entry point to all things kewl.
+// A lot of the stuff in this file can be extracted to another file, but for
+// early prototyping it's all here.
+
+//======================================================================================================================
+// Includes
+//======================================================================================================================
 #include "cinder/ImageIo.h"
 #include "cinder/Rand.h"
 #include "cinder/Text.h"
@@ -11,7 +20,11 @@
 #include "cinder/gl/Texture.h"
 #include "cinder/gl/VboMesh.h"
 #include "cinder/gl/gl.h"
+#include "resources.h"
 
+//======================================================================================================================
+// Namespaces
+//======================================================================================================================
 using namespace ci;
 using namespace ci::app;
 using namespace std;
@@ -20,68 +33,121 @@ using namespace std;
 class cKeyHandler {
 public:
   void Handle(KeyEvent event, bool isDown) {
-    if (event.getCode() == KeyEvent::KEY_UP) {
-      Up = isDown;
-    }
-    if (event.getCode() == KeyEvent::KEY_LEFT) {
-      Left = isDown;
-    }
-    if (event.getCode() == KeyEvent::KEY_RIGHT) {
-      Right = isDown;
-    }
-    if (event.getCode() == KeyEvent::KEY_DOWN) {
-      Down = isDown;
-    }
+    auto const k_code = event.getCode();
+
+    // Escape is only on key up
+    Escape = k_code == KeyEvent::KEY_ESCAPE && !isDown;
+
+    // All of these are really 'is key being held down'
+    if (k_code == KeyEvent::KEY_UP) { Up = isDown; }
+    if (k_code == KeyEvent::KEY_LEFT) { Left = isDown; }
+    if (k_code == KeyEvent::KEY_RIGHT) { Right = isDown; }
+    if (k_code == KeyEvent::KEY_DOWN) { Down = isDown; }
+    if (k_code == KeyEvent::KEY_SPACE) { Space = isDown; }
   }
 
+  bool Escape{false};
   bool Left{false};
   bool Right{false};
   bool Up{false};
   bool Down{false};
+  bool Space{false};
 };
 
 //======================================================================================================================
 class cEntity {
 public:
-  static const uint32_t k_DefaultSpeed = 8;
+  virtual ~cEntity() = default;
 
-  //======================================================================================================================
-  cEntity() = default;
-
-  //======================================================================================================================
-  void Draw(gl::BatchRef batch) {
-    gl::pushModelMatrix();
-    gl::ScopedColor color(CurrentColor);
-    gl::translate(Position);
-    batch->draw();
-    gl::popModelMatrix();
-  }
-
-  //======================================================================================================================
-  void Update(cKeyHandler const &keys) {
-    auto const k_WindowSize = getWindowSize();
-    if (keys.Right) {
-      Position.x = (Position.x + Radius >= k_WindowSize.x) ? Position.x : (Position.x + k_DefaultSpeed);
-    }
-
-    if (keys.Left) {
-      Position.x = (Position.x - Radius <= 0) ? Position.x : (Position.x - k_DefaultSpeed);
-    }
-
-    if (keys.Up) {
-      Position.y = (Position.y - Radius <= 0) ? Position.y : (Position.y - k_DefaultSpeed);
-    }
-
-    if (keys.Down) {
-      Position.y = (Position.y + Radius >= k_WindowSize.y) ? Position.y : (Position.y + k_DefaultSpeed);
-    }
-  }
-
-  vec2 Position{0, 0};
-  float Radius{30.0f};
-  Color8u CurrentColor{239, 225, 71};
+  vec2 Velocity;
+  vec2 Position;
+  Color8u Color;
+  float Size;
 };
 
+//======================================================================================================================
+// The default entity, right now it's always a circle and has no hit detection.
+//======================================================================================================================
+class cPlayer : public cEntity {
+public:
+  static const uint32_t k_DefaultSpeed = 6;
+
+  //======================================================================================================================
+  cPlayer() = default;
+
+  //======================================================================================================================
+  void Draw() const {
+    gl::ScopedModelMatrix m;
+    gl::ScopedColor c(Color);
+
+    // Draw a simple colored rectangle, don't actully use the textures (yet)
+    gl::translate(Position);
+    gl::drawSolidRect(Rectf(0, 0, Size, Size));
+  }
+
+  //======================================================================================================================
+  void Update(cKeyHandler const & keys) {
+    auto const k_WindowSize = getWindowSize();
+
+    if (keys.Right) { Position.x += k_DefaultSpeed; }
+    if (keys.Left) { Position.x -= k_DefaultSpeed; }
+    if (keys.Up) { Position.y -= k_DefaultSpeed; }
+    if (keys.Down) { Position.y += k_DefaultSpeed; }
+
+    auto const k_xMax = k_WindowSize.x - Size;
+    auto const k_yMax = k_WindowSize.y - Size;
+    Position.x        = (Position.x <= 0) ? 0 : ((Position.x >= k_xMax) ? k_xMax : Position.x);
+    Position.y        = (Position.y <= 0) ? 0 : ((Position.y >= k_yMax) ? k_yMax : Position.y);
+  }
+};
+
+//======================================================================================================================
+class cBullet : public cEntity {
+public:
+  //======================================================================================================================
+  void Draw() const {
+    gl::ScopedModelMatrix m;
+    gl::ScopedColor c(Color);
+
+    // Draw a simple colored rectangle, don't actully use the textures (yet)
+    gl::translate(Position);
+    gl::drawSolidRect(Rectf(0, 0, Size, Size));
+  }
+
+  //======================================================================================================================
+  void Update() {
+    Position += Velocity;
+    if (Position.y < 0) { Dead = true; }
+  }
+
+  bool Dead {false};
+};
+
+//======================================================================================================================
+class cEnemy : public cEntity {
+public:
+  //======================================================================================================================
+  void Draw() const {
+    gl::ScopedModelMatrix m;
+    gl::ScopedColor c(Color);
+
+    // Draw a simple colored rectangle, don't actully use the textures (yet)
+    gl::translate(Position);
+    gl::drawSolidRect(Rectf(0, 0, Size, Size));
+  }
+
+  //======================================================================================================================
+  void Update() {
+    auto const k_ws = getWindowSize();
+    Position += Velocity;
+  }
+
+  bool Dead;
+};
+
+//======================================================================================================================
+// The A GAME App!
+// Does meaningful work...
 //======================================================================================================================
 class cGame : public App {
 public:
@@ -91,21 +157,28 @@ public:
   void draw() override;
   void keyDown(KeyEvent) override;
   void keyUp(KeyEvent) override;
+  static void prepare(Settings *);
 
 private:
   // mesh and texture
-  gl::VboMeshRef Mesh;
-  gl::BatchRef Batch;
+  gl::BatchRef Circle;
   gl::TextureRef Texture;
 
   // Default shader
   gl::GlslProgRef Shader;
 
   cKeyHandler Keys;
-  cEntity Player;
+  cPlayer Player;
+  std::vector< cBullet > Bullets;
 };
 
 const Color cGame::k_BackgroundColor = Color(0.11f, 0.112f, 0.11f);
+
+//======================================================================================================================
+void cGame::prepare(Settings * settings) {
+  settings->setTitle("A GAME");
+  settings->setWindowSize(500, 500);
+}
 
 //======================================================================================================================
 void cGame::keyDown(KeyEvent event) { Keys.Handle(event, true); }
@@ -114,55 +187,42 @@ void cGame::keyDown(KeyEvent event) { Keys.Handle(event, true); }
 void cGame::keyUp(KeyEvent event) { Keys.Handle(event, false); }
 
 //======================================================================================================================
+// Our main 'do a thing' function, where all of our setup and updates are
+// actually seen!
+//======================================================================================================================
+void cGame::draw() {
+  gl::clear(k_BackgroundColor);
+  gl::setMatricesWindow(getWindowSize());
+
+  for (auto const & b : Bullets) {
+    b.Draw();
+  }
+  Player.Draw();
+}
+
+//======================================================================================================================
 void cGame::setup() {
-  getWindow()->setTitle("A GAME");
   // allow maximum frame rate
   disableFrameRate();
   gl::enableVerticalSync(false);
 
-  // BEGIN CODE I KNOW NOTHING ABOUT
-  
+  // Player config
+  Player.Position = getWindowCenter();
+  Player.Size     = 15;
+  Player.Color    = Color8u(255, 235, 59);
+
+  // load texture
+  Texture = gl::Texture::create(loadImage(loadAsset(TEXTURE_PLAYER)));
+  Texture->bind();
+
   // create a default shader with color and texture support
-  Shader = gl::context()->getStockShader(gl::ShaderDef().color().texture());
-
-  // create ball mesh ( much faster than using gl::drawSolidCircle() )
-  size_t const slices = 20;
-
-  std::vector<vec3> positions;
-  std::vector<vec2> texcoords;
-  std::vector<uint8_t> indices;
-
-  texcoords.push_back(vec2(0.5f, 0.5f));
-  positions.push_back(vec3(0));
-
-  for (size_t i = 0; i <= slices; ++i) {
-    float angle = i / (float)slices * 2.0f * (float)M_PI;
-    vec2 v(sinf(angle), cosf(angle));
-
-    texcoords.push_back(vec2(0.5f, 0.5f) + 0.5f * v);
-    positions.push_back(Player.Radius * vec3(v, 0.0f));
-  }
-
-  gl::VboMesh::Layout layout;
-  layout.usage(GL_STATIC_DRAW);
-  layout.attrib(geom::Attrib::POSITION, 3);
-  layout.attrib(geom::Attrib::TEX_COORD_0, 2);
-
-  Mesh = gl::VboMesh::create(positions.size(), GL_TRIANGLE_FAN, {layout});
-  Mesh->bufferAttrib(geom::POSITION, positions.size() * sizeof(vec3),
-                     positions.data());
-  Mesh->bufferAttrib(geom::TEX_COORD_0, texcoords.size() * sizeof(vec2),
-                     texcoords.data());
+  auto shader = gl::ShaderDef().color().texture();
+  Shader      = getStockShader(shader);
 
   // combine mesh and shader into batch for much better performance
-  Batch = gl::Batch::create(Mesh, Shader);
-
-  // END CODE I KNOW NOTHING ABOUT
-  
-  // load texture
-  Texture = gl::Texture::create(loadImage(loadAsset(BALL_TEXTURE)));
-
-  Player.Position = getWindowCenter();
+  // auto circle = geom::Circle().subdivisions(40).radius(15.0f);
+  auto shape = geom::Rect().rect(Rectf(0, 0, Player.Size, Player.Size));
+  Circle     = gl::Batch::create(shape, Shader);
 }
 
 //======================================================================================================================
@@ -171,7 +231,8 @@ void cGame::update() {
   static const double timestep = 1.0 / 60.0;
 
   // Keep track of time.
-  static double time = getElapsedSeconds();
+  static double time        = getElapsedSeconds();
+  static double lastBullet  = 0.0;
   static double accumulator = 0.0;
 
   // Calculate elapsed time since last frame.
@@ -179,29 +240,35 @@ void cGame::update() {
   time += elapsed;
 
   // Update the simulation.
-  accumulator += math<double>::min(elapsed, 0.1); // prevents 'spiral of death'
+  accumulator += math< double >::min(elapsed, 0.1); // prevents 'spiral of death'
   while (accumulator >= timestep) {
     accumulator -= timestep;
     Player.Update(Keys);
+    for (auto itr = std::begin(Bullets); itr != std::end(Bullets);) {
+      if (itr->Dead) { itr = Bullets.erase(itr); }
+      else {
+        itr->Update();
+        itr++;
+      }
+    }
+
+    console() << Bullets.size() << " bullets" << std::endl;
+
+    if (Keys.Escape) { quit(); }
+
+    if (Keys.Space && (time - lastBullet) > (5 * timestep)) {
+      cBullet bullet;
+      bullet.Position = Player.Position + vec2(Player.Size * 0.5, 0);
+      bullet.Size     = 2;
+      bullet.Velocity = {0, -7};
+      bullet.Color    = Color(1, 0, 0);
+      Bullets.push_back(bullet);
+      lastBullet = time;
+    }
   }
 }
 
 //======================================================================================================================
-void cGame::draw() {
-  gl::clear(k_BackgroundColor);
-
-  gl::ScopedBlendAdditive blend;
-  gl::ScopedGlslProg shader(Shader);
-  Shader->uniform("uTex0", 0);
-
-  gl::ScopedTextureBind tex0(Texture);
-
-  Player.Draw(Batch);
-}
-
-
-//======================================================================================================================
 // This is a magical macro that sets up our 2D world and runs our app!
 //======================================================================================================================
-CINDER_APP(cGame, RendererGl)
-
+CINDER_APP(cGame, RendererGl, &cGame::prepare)
